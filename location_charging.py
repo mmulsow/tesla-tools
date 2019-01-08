e69e75a43c8191e80ae4ac9756a625cb61cf7fc7
Matt Mulsow <matt.mulsow@gmail.com>
2019-01-08 17:41:10 +0000

    Updated to read from config
    Signed-off-by: Matt Mulsow <matt.mulsow@gmail.com>

--- location_charging.py
+++ location_charging.py
@@ -2,20 +2,9 @@
 
 from math import sin, cos, sqrt, atan2, radians
 import http.client
-import getpass
 import json
 import time
-
-CLIENT_ID = '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384'
-CLIENT_SCT = 'c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3'
-
-HOME = {
-    "latitude": 30.3978822,
-    "longitude": -97.7145154,
-}
-
-HOME_TARGET = 80
-AWAY_TARGET = 90
+import yaml
 
 
 class TeslaAPIError(Exception):
@@ -25,43 +14,70 @@ class TeslaAPIError(Exception):
 class TeslaAPI:
     USER_AGENT = 'Python 3'
 
-    def __init__(self, email, password):
-        self._email = email
-        self._conn = http.client.HTTPSConnection('owner-api.teslamotors.com')
-        self._auth_data = self.auth(email, password)
+    def __init__(self):
+        self.access_token = None
+        self._conn = None
 
     def _post_request(self, url, data):
-        body = json.dumps(data)
+        for i in range(3):
+            try:
+                body = json.dumps(data)
 
-        headers = {
-            'User-Agent': self.USER_AGENT,
-            'Content-Type': 'application/json'
-        }
+                headers = {
+                    'User-Agent': self.USER_AGENT,
+                    'Content-Type': 'application/json'
+                }
 
-        if hasattr(self, '_auth_data'):
-            headers['Authorization'] = \
-                'Bearer ' + self._auth_data['access_token']
+                if self.access_token:
+                    headers['Authorization'] = 'Bearer ' + self.access_token
 
-        self._conn.request('POST', url, body, headers)
+                if self._conn is None:
+                    self._conn = http.client.HTTPSConnection('owner-api.teslamotors.com')
+                self._conn.request('POST', url, body, headers)
 
-        res = self._conn.getresponse()
-        if res.status != 200:
-            raise TeslaAPIError('Error %s: %r' % (res.status, res.read()))
+                res = self._conn.getresponse()
+                if res.status != 200:
+                    raise TeslaAPIError('Error %s: %r' % (res.status, res.read()))
 
-        return json.load(res)
+                ret = json.load(res)
+                break
+            except Exception as e:
+                if i == 2:
+                    raise
+
+                print(time.ctime(), 'Error "%s". trying again...' % (e,))
+                self._conn = None
+                time.sleep(10)
+
+        return ret
 
     def _get_request(self, url):
-        headers = {
-            'User-Agent': self.USER_AGENT,
-            'Authorization': 'Bearer %s' % self._auth_data['access_token']
-        }
+        for i in range(3):
+            try:
+                headers = {
+                    'User-Agent': self.USER_AGENT,
+                    'Authorization': 'Bearer %s' % self.access_token
+                }
 
-        self._conn.request('GET', url, headers=headers)
-        res = self._conn.getresponse()
-        if res.status != 200:
-            raise TeslaAPIError('Error %s: %r' % (res.status, res.read()))
+                if self._conn is None:
+                    self._conn = http.client.HTTPSConnection('owner-api.teslamotors.com')
+                self._conn.request('GET', url, headers=headers)
 
-        return json.load(res)['response']
+                res = self._conn.getresponse()
+                if res.status != 200:
+                    raise TeslaAPIError('Error %s: %r' % (res.status, res.read()))
+
+                ret = json.load(res)['response']
+                break
+            except Exception as e:
+                if i == 2:
+                    raise
+
+                print(time.ctime(), 'Error "%s". trying again...' % (e,))
+                self._conn = None
+                time.sleep(10)
+
+        return ret
 
     def auth(self, email, password):
         data = {
@@ -110,40 +126,50 @@ def calc_dist(p1, p2):
 
 
 def main():
-    email = input('E-mail: ')
-    password = getpass.getpass()
+    with open('config.yml') as fp:
+        config = yaml.load(fp)
 
-    t = TeslaAPI(email, password)
-    vehicle_id = t.vehicles()[0]['id']
+    t = TeslaAPI()
 
-    changed = False
+    changed = {}
+    for user in config['users']:
+        changed[user] = False
+
     while True:
-        charge_state = t.charge_state(vehicle_id)
-        if charge_state['charging_state'] == 'Charging':
-            if changed:
-                continue
+        for user in config['users']:
+            print(time.ctime(), 'Checking for user', user)
 
-            drive_state = t.drive_state(vehicle_id)
-            distance = calc_dist(HOME, drive_state)
-            if distance < 0.1:
-                print('Charging at home to %s%%' %
-                      (charge_state['charge_limit_soc'],))
-                if charge_state['charge_limit_soc'] != HOME_TARGET:
-                    print('Changing charge target from %s to %s at home.' %
-                          (charge_state['charge_limit_soc'], HOME_TARGET))
-                    t.set_charge_limit(vehicle_id, HOME_TARGET)
-                    changed = True
+            t.access_token = config['users'][user]['access_token']
+            home = config['users'][user]['home']
+            home_target = config['users'][user]['targets']['home']
+            away_target = config['users'][user]['targets']['away']
+            vehicle_id = config['users'][user]['vehicle_id']
+
+            charge_state = t.charge_state(vehicle_id)
+            if charge_state['charging_state'] in ['Charging', 'Complete']:
+                if changed[user]:
+                    continue
+                changed[user] = True
+
+                drive_state = t.drive_state(vehicle_id)
+                distance = calc_dist(home, drive_state)
+                if distance < 0.1:
+                    print(time.ctime(), 'Charging at home to %s%%' %
+                          (charge_state['charge_limit_soc'],))
+                    if charge_state['charge_limit_soc'] != home_target:
+                        print(time.ctime() + ' Changing charge target from %s to %s at home.' %
+                              (charge_state['charge_limit_soc'], home_target))
+                        t.set_charge_limit(vehicle_id, home_target)
+                else:
+                    print(time.ctime(), 'Charging %s km from home to %s%%' %
+                          (distance, charge_state['charge_limit_soc']))
+                    if charge_state['charge_limit_soc'] != away_target:
+                        print(time.ctime(), 'Changing charge target from %s to %s for away.' %
+                              (charge_state['charge_limit_soc'], away_target))
+                        t.set_charge_limit(vehicle_id, away_target)
             else:
-                print('Charging %s km from home to %s%%' %
-                      (distance, charge_state['charge_limit_soc']))
-                if charge_state['charge_limit_soc'] != AWAY_TARGET:
-                    print('Changing charge target from %s to %s at home.' %
-                          (charge_state['charge_limit_soc'], AWAY_TARGET))
-                    t.set_charge_limit(vehicle_id, AWAY_TARGET)
-                    changed = True
-        else:
-            print('Not charging')
-            changed = False
+                print(time.ctime(), 'Not charging')
+                changed[user] = False
 
         time.sleep(60)
 
